a5744a1bf2ccef372d15ddd4ffffcafc134477b5
Faraz <farazprottoy@gmail.com>
2024-04-22 16:27:48 +0600

    - store created

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Task from "./Task";
 
 const tasks = [
   {
@@ -24,7 +25,13 @@ export default function Column({
     <section className="h-[600px] flex-1">
       <h2 className="ml-1 font-serif text-2xl font-semibold">{title}</h2>
 
-      <div className="mt-3.5"></div>
+      <div className="mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4">
+        <div className="flex flex-col gap-4">
+          {filteredTasks.map((task) => {
+            return <Task key={task.id} {...task} />;
+          })}
+        </div>
+      </div>
     </section>
   );
 }
